fa333f20bd3c17f4965c132d8e9ee42387991441
Code-Nostra <artem2002228228@mail.ru>
2025-01-25 21:22:37 +0300

    feat: add creating images of QR codes

---
+++ DynamicQR/QR-code/Endpoint/CreateQRImage.cs
@@ -0,0 +1,35 @@
+﻿using DynamicQR.Common;
+using DynamicQR.Common.Api.Filters;
+using DynamicQR.QR_code.Services;
+
+namespace DynamicQR.QR_code.Endpoint
+{
+	public class CreateQRImage : IEndpoint
+	{
+		public static void Map(IEndpointRouteBuilder app)
+		{
+			app.MapPost("/CreateQrImage", Handle)
+				.WithSummary("Get QR code images")
+				.WithRequestValidation<Request>();
+		}
+
+		public record Request(string baseUrl, int UserId,int QrId);
+
+		public class RequestValidation : AbstractValidator<Request>
+		{
+			public RequestValidation()
+			{
+				RuleFor(x => x.UserId).GreaterThan(0);
+				RuleFor(x => x.QrId).GreaterThan(0);
+			}
+		}
+
+		public record Response(string byteArr);
+		public static Ok<Response> Handle(Request request, DynamicQrContext database)
+		{
+			string url = $"{request.baseUrl}/{request.UserId}/{request.QrId}";
+
+			return TypedResults.Ok(new Response(QrGenerator.GenerateQrAsBase64(url)));
+		}
+	}
+}
