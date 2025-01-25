8393c9ff783158dace0cc9091785d3f5a210a601
Code-Nostra <artem2002228228@mail.ru>
2025-01-25 21:25:15 +0300

    feat: add Title field to File

--- DynamicQR/File/Endpoint/UploadFile.cs
+++ DynamicQR/File/Endpoint/UploadFile.cs
@@ -1,48 +1,46 @@
-﻿using DynamicQR.Authentication.Services;
-using DynamicQR.Common;
-using DynamicQR.Common.Api.Filters;
+﻿using DynamicQR.Common;
 using Microsoft.AspNetCore.Mvc;
-using System.Reflection.PortableExecutable;
 
 namespace DynamicQR.File.Endpoint
 {
-    public class UploadFile : IEndpoint
-    {
-        public static void Map(IEndpointRouteBuilder app)
-        {
-            app.MapPost("/UploadFile", Handle)
-                .WithRequestValidation<Request>()
+	public class UploadFile : IEndpoint
+	{
+		public static void Map(IEndpointRouteBuilder app)
+		{
+			app.MapPost("/UploadFile", Handle)
+				.WithRequestValidation<Request>()
 				.DisableAntiforgery();
 
 		}
 
-        public record Request(IFormFile file);
+		public record Request(IFormFile file);
 
 		public class RequestValidator : AbstractValidator<Request>
 		{
 			public RequestValidator()
 			{
-                RuleFor(x => x.file.Length).GreaterThan(0).LessThan(10*1024*1024);
+				RuleFor(x => x.file.Length).GreaterThan(0).LessThan(10 * 1024 * 1024);
 			}
 		}
 
-		public static async Task<Ok> Handle([FromForm] Request request, DynamicQrContext database,ClaimsPrincipal claimsPrincipal, CancellationToken cancellationToken)
-        {
-            using (var ms = new MemoryStream())
-            {
-                await request.file.CopyToAsync(ms,cancellationToken);
-                var fileBytes = ms.ToArray();
+		public static async Task<Ok> Handle([FromForm] Request request, DynamicQrContext database, ClaimsPrincipal claimsPrincipal, CancellationToken cancellationToken)
+		{
+			using (var ms = new MemoryStream())
+			{
+				await request.file.CopyToAsync(ms, cancellationToken);
+				var fileBytes = ms.ToArray();
 
 				var file = new FileQr
 				{
 					File = fileBytes,
+					Title = request.file.FileName,
 					UserId = claimsPrincipal.GetUserId()
 				};
-				await database.FileQrs.AddAsync(file,cancellationToken);
+				await database.FileQrs.AddAsync(file, cancellationToken);
 			}
-            await database.SaveChangesAsync(cancellationToken);
-            return TypedResults.Ok();
-        }
-        
-    }
+			await database.SaveChangesAsync(cancellationToken);
+			return TypedResults.Ok();
+		}
+
+	}
 }
